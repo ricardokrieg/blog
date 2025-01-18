d56785f519ec93be98caade2f35cac350acf0fab
Ricardo Andrade <ricardo.krieg@gmail.com>
2025-01-18 10:06:30 -0300

    chore: dont specify the mongo version for tests

--- backend/src/test/globalSetup.js
+++ backend/src/test/globalSetup.js
@@ -1,11 +1,7 @@
 import { MongoMemoryServer } from "mongodb-memory-server"
 
 export default async function globalSetup() {
-  const instance = await MongoMemoryServer.create({
-    binary: {
-      version: "6.0.20",
-    },
-  })
+  const instance = await MongoMemoryServer.create()
 
   global.__MONGOINSTANCE = instance
   process.env.DATABASE_URL = instance.getUri()
